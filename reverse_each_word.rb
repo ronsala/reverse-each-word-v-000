71d1e5a2f830e10ea657272953c8436f581433af
Ron Sala <ron.sala@gmail.com>
2017-12-20 08:19:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,33 @@
+string = "Hello there, and how are you?"
+
+def reverse_each_word(string)
+  words = string.split(/ /)
+  new_sentence = []
+  words.each do |word|
+		end_loop = word.length
+		new_string = ''
+		while end_loop > 0
+			end_loop -= 1
+			new_string += word[end_loop]
+		end
+		new_sentence << new_string
+	end
+	new_sentence.join(' ')
+end
+
+reverse_each_word(string)
+
+def reverse_each_word(string)
+  words = string.split(/ /)
+  new_sentence = []
+  words.collect do |word|
+		end_loop = word.length
+		new_string = ''
+		while end_loop > 0
+			end_loop -= 1
+			new_string += word[end_loop]
+		end
+		new_sentence << new_string
+	end
+	new_sentence.join(' ')
+end
