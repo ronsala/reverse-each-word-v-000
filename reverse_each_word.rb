1576c7366c4c961253305ef2e36f6671b58e8278
Ron Sala <ron.sala@gmail.com>
2017-12-20 08:06:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,14 @@
 def reverse_each_word(string)
-  string.split(/ /)
+  words = string.split(/ /)
+  new_sentence = []
+  words.each do |word|
+		end_loop = word.length
+		new_string = ''
+		while end_loop > 0
+			end_loop -= 1
+			new_string += word[end_loop]
+		end
+		new_sentence << new_string
+	end
+	new_sentence.join(' ')
 end
